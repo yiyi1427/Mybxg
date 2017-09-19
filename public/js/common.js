7d68b686430ba3e25e0c5d0340004c6cea1be150
yiyi1427 <446551895@qq.com>
2017-09-19 08:13:05 +0800

    完善登录退出功能

--- public/js/common.js
+++ public/js/common.js
@@ -1,8 +1,35 @@
+NProgress.start();
 
-	NProgress.start();
+NProgress.done();
 
-	NProgress.done();
+$('.navs ul').prev('a').on('click', function () {
+	$(this).next().slideToggle();
+});
 
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
-	});
+//退出功能的实现
+$("#logoutBtn").on('click', function () {
+	$.ajax({
+		type: 'post',
+		url: '/api/logout',
+		dataType: 'json',
+		success: function (data) {
+			if (data.code == 200) {
+				location.href = '/main/login';
+			}
+		}
+	})
+	//console.log(location.href);
+	console.log(123);
+})
+//验证是否登录了
+var flag = $.cookie('PHPSESSID');
+if (!flag) {
+	location.href = '/main/login';
+}
+//填充头像信息
+var loginInfo = $.cookie('logInfo');
+console.log(loginInfo);
+loginInfo = loginInfo && JSON.parse(loginInfo);
+//console.log(loginInfo);
+$(".aside .profile img").attr('src', loginInfo.tc_avatar);
+$(".aside .profile h4").html(loginInfo.tc_name);
